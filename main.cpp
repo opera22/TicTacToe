ace9f931deeb0cc85a1824ba4c33e758d3d07b3a
Drew <andrewburns.r@gmail.com>
2020-11-07 11:24:17 -0700

    created board

--- main.cpp
+++ main.cpp
@@ -4,12 +4,29 @@ using namespace std;
 int main() {
 
 
-    cout << "Hello world!" << endl;
+    cout << "Welcome to TicTacToe!" << endl;
+    cout << "---------------------" << endl;
+    cout << endl;
+    
+    cout << "1    |2    |3          " << endl;
+    cout << "     |     |           " << endl;
+    cout << "_____|_____|_____      " << endl;
+    cout << "4    |5    |6          " << endl;
+    cout << "     |     |           " << endl;
+    cout << "_____|_____|_____      " << endl;
+    cout << "7    |8    |9          " << endl;
+    cout << "     |     |           " << endl;
+    cout << "     |     |           " << endl;
+    
+    
+    
+    
+    /*cout << "  _|O|_       _|_|_  " << endl;
+    cout << "  O|X|X       _|_|_  " << endl;
+    cout << "   |O|         | |   " << endl;
+    cout << endl;*/
 
-    int age = 0;
-    cout << "Your age (in years): " << endl;
-    cin >> age;
-    cout << "You are " << age << " years old!" << endl;
+    
 
     return 0;
 
