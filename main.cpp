6fe2ce8bfb5a339448bfaedcd2e9d8975dca13a6
Drew <andrewburns.r@gmail.com>
2020-11-07 10:44:12 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    cout << "Hello world!" << endl;
+
+    int age = 0;
+    cout << "Your age (in years): " << endl;
+    cin >> age;
+    cout << "You are " << age << " years old!" << endl;
+
+    return 0;
+
+}
